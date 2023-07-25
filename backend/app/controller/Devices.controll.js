db1d9ea6650e05901d5d961800deb17b310b4d4a
Pedro Eduardo Camera <pedro.eduardo.camera@gmail.com>
2023-07-25 01:30:00 -0300

    aa

--- backend/app/controller/Devices.controll.js
+++ backend/app/controller/Devices.controll.js
@@ -2,6 +2,16 @@ import { Devices_Model } from '../model/Devices.model.js'
 
 
 
+/**
+ * The function FindAllDevices retrieves all devices from the Devices_Model and sends them as a JSON
+ * response.
+ * @param req - The `req` parameter is the request object that contains information about the HTTP
+ * request made by the client. It includes details such as the request method, headers, query
+ * parameters, and body.
+ * @param res - The "res" parameter is the response object that is used to send the response back to
+ * the client. It contains methods and properties that allow you to control the response, such as
+ * setting the status code and sending JSON data.
+ */
 
 export const FindAllDevices = async (req,res) => {
     try{
@@ -16,6 +26,16 @@ export const FindAllDevices = async (req,res) => {
 
 
 
+/**
+ * The function `getDevicesById` retrieves a device by its ID from the database and sends it as a JSON
+ * response, or returns an error message if there is an issue with the database connection.
+ * @param req - The `req` parameter is the request object that contains information about the incoming
+ * HTTP request, such as the request headers, request parameters, and request body. In this case,
+ * `req.params.id` is used to extract the `id` parameter from the request URL.
+ * @param res - The `res` parameter is the response object that is used to send the response back to
+ * the client. It contains methods and properties that allow you to control the response, such as
+ * setting the status code and sending JSON data.
+ */
 export const getDevicesById = async (req,res) => {
     let id = req.params.id;
 
@@ -30,6 +50,15 @@ export const getDevicesById = async (req,res) => {
 
 
 
+/**
+ * The function creates a new device by saving the provided device information in the database and
+ * returns the newly created device data.
+ * @param req - The `req` parameter is the request object that contains information about the incoming
+ * HTTP request, such as the request headers, request body, and request parameters.
+ * @param res - The `res` parameter is the response object that is used to send the response back to
+ * the client. It contains methods and properties that allow you to control the response, such as
+ * setting the status code and sending data back to the client.
+ */
 
 export const createDevice = async (req,res) => {
 
